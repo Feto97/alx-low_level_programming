885760be2968138f264d88ca37ec97542398d170
Feto97 <111251778+Feto97@users.noreply.github.com>
2022-09-08 10:00:26 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-/* 59 character*/
+/**
+ * main - Prints out the pharase.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
