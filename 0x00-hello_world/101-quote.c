ee5777e40c562c0ee98490f7bacfdb7e902bf1ee
Feto97 <111251778+Feto97@users.noreply.github.com>
2022-09-08 09:57:57 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
+/* 59 character*/
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
