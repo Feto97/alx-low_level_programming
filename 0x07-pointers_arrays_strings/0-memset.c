20173ed99edb431b6dc118b0574aee5fa5944024
Feto97 <111251778+Feto97@users.noreply.github.com>
2022-09-27 01:03:55 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - function that fills memory with a constant byte.
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * @s: mamory area to return
- * @b: constant byte
- * @n: size of bytes
- * Return: char
+ * Return: Nothing.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+        unsigned int i;
 
-	while (i < n)
-	{
-
-		*(s + i) = b;
-		i += 1;
-	}
-	return (s);
+        i = 0;
+        while (i < n)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", s[i]);
+                i++;
+        }
+        printf("\n");
 }
