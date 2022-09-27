c2a69de120acc8d1d90898c44a1240326a0b917d
Feto97 <111251778+Feto97@users.noreply.github.com>
2022-09-27 02:37:42 +0200

    mandatory task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,18 @@
-#include "main.h"
+#include "stdio.h"
 /**
- * _memset - fills memory with a constant byte.
- * @s: source string
- * @b: the contant byte for filling
- * @n: lenght of buffer
- * Return: new string.
- */
+* _memset - fills memory with a constant byte
+* @s: memory space to be filled
+* @b: byte to fill with
+* @n: number of spaces to fill
+*
+* Return: pointer to memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		*(s + i) = b;
-		i++;
-	}
-	return (s);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
+}
+return (s);
 }
